738d9aabbe2522a11b0acb0bea35e8076046f9b6
Luis Saes <saeslhs@gmail.com>
2023-02-26 20:08:25 -0300

    Update code to use destructuring and update usage of service

--- src/controllers/validatePasswordController.ts
+++ src/controllers/validatePasswordController.ts
@@ -2,16 +2,14 @@ import { Request, Response } from "express";
 import { ValidatePasswordService } from "../services/validatePasswordService";
 import { ValidatePasswordRequestBody, ValidatePasswordResponseBody } from "../types/validatePasswordRequest";
 
-const validatePasswordService = new ValidatePasswordService();
-
 function validatePassword(req: Request<ValidatePasswordRequestBody, ValidatePasswordResponseBody>, res: Response) {
-  const password: string = req.body.password;
+  const { password }: { password: string } = req.body;
 
   if (!password) {
     return res.status(400).send("Empty password, please provide a password");
   }
 
-  const isValid: boolean = validatePasswordService.isValidPassword(password);
+  const isValid: boolean = new ValidatePasswordService().isValidPassword(password);
 
   return res.json({ isValid });
 }
