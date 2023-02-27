a2dddd764fc168545131ab926c9908595bad7937
Luis Saes <saeslhs@gmail.com>
2023-02-27 12:15:06 -0300

    Update unit tests to have 100% coverage

--- src/services/__test__/validatePasswordService.test.ts
+++ src/services/__test__/validatePasswordService.test.ts
@@ -8,48 +8,21 @@ describe("ValidatePasswordService", () => {
     expect(isValid).toBe(false);
   });
 
-  const casesWithoutDigit = [
-    "abcdefgh",
-    "ABCDEFGH",
-    "abcdefghijk",
-    "abcdefghijklmno",
-    "Qwerty!@#",
-    "ABCdefGHIjklMNO!@#",
-    "GHIJKLMNOPQWERTY",
-    "GHIJKLMNOPQWERTY!",
-  ];
+  const casesWithoutDigit = ["abcdefghiRH@", "qwert!yuioPL", "zaqxs#wcdeQE", "vfebgr$nhtSW", "lpkoji(SWEAR"];
   test.each(casesWithoutDigit)("Passwords without digit", (password) => {
     const isValid = new ValidatePasswordService().isValidPassword(password);
 
     expect(isValid).toBe(false);
   });
 
-  const casesWithoutLowercase = [
-    "12345678",
-    "ABCDEFGH",
-    "1234567890",
-    "ABCDEFGHIJKLMNO",
-    "QWERTY!@#",
-    "ABCDEFGHIJKLMNO!@#",
-    "GHIJKLMNOPQWERTY",
-    "GHIJKLMNOPQWERTY!",
-  ];
+  const casesWithoutLowercase = ["ABCDE90FGHI@", "QWE17RT!YUIO", "ZAQXS32#WCDE", "VFEBGR$NHT56", "LPKOJI(12345"];
   test.each(casesWithoutLowercase)("Passwords without lowercase", (password) => {
     const isValid = new ValidatePasswordService().isValidPassword(password);
 
     expect(isValid).toBe(false);
   });
 
-  const casesWithoutUppercase = [
-    "12345678",
-    "abcdefgh",
-    "1234567890",
-    "abcdefghijklmno",
-    "Qwerty!@#",
-    "abcdefghijklmno!@#",
-    "GHIJKLMNOPQWERTY",
-    "GHIJKLMNOPQWERTY!",
-  ];
+  const casesWithoutUppercase = ["abcde90fghi@", "qwe17rt!yuio", "zaqxs32#wcde", "vfebgr$nht56", "lpkoji(12345"];
   test.each(casesWithoutUppercase)("Passwords without uppercase", (password) => {
     const isValid = new ValidatePasswordService().isValidPassword(password);
 
@@ -57,14 +30,11 @@ describe("ValidatePasswordService", () => {
   });
 
   const casesWithoutSpecialCharacter = [
-    "12345678",
-    "abcdefgh",
-    "1234567890",
-    "abcdefghijklmno",
-    "Qwertyyyyy",
-    "ABCdefGHIjklMNO",
-    "GHIJKLMNOPQWERTY",
-    "GHIJKLMNOPQWERTYU",
+    "abWcde90fghiRS",
+    "qwe17WSrtyuio",
+    "zaqxERs32wcde",
+    "vfebgrKQWnht56",
+    "lpkojiA12345",
   ];
   test.each(casesWithoutSpecialCharacter)("Passwords without special character", (password) => {
     const isValid = new ValidatePasswordService().isValidPassword(password);
@@ -73,14 +43,11 @@ describe("ValidatePasswordService", () => {
   });
 
   const casesWithWhitespace = [
-    "12345678 ",
-    " abcdefgh",
-    "1234567890 ",
-    " abcdefghijklmno",
-    "Qwerty!@# ",
-    "ABCdefGHIjklMNO!@#",
-    "GHIJKLMNOPQWERTY ",
-    "GHIJKLMNOPQWERTY! ",
+    "a  bWcde@!90fgh  iRS",
+    "qwe17(WSrtyuio ",
+    "zaqxER )s32wcde",
+    " vfebgrK#QWnht56",
+    "lpk oj i A12 345+-",
   ];
   test.each(casesWithWhitespace)("Passwords with whitespace", (password) => {
     const isValid = new ValidatePasswordService().isValidPassword(password);
@@ -89,14 +56,11 @@ describe("ValidatePasswordService", () => {
   });
 
   const casesWithRepeatedCharacters = [
-    "112345678",
-    "aabcdefgh",
-    "11234567890",
-    "aabcdefghijklmno",
-    "Qwertyy!@#",
-    "ABCdefGHIjklMN!O!@#",
-    "GHIJKLMNOPQWWERTY",
-    "GHIJKLKMNOPQWERTY!",
+    "abWcde@!90feRSghiRS",
+    "qwqe17(WSrtyuio",
+    "zaqxER)s32wc)de",
+    "vfeffffffffbgrK#QWnht56",
+    "lplkojiA12345+-",
   ];
   test.each(casesWithRepeatedCharacters)("Passwords with repeated characters", (password) => {
     const isValid = new ValidatePasswordService().isValidPassword(password);
